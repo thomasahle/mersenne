68ca1a5b8f030fd0d9142e9d81530a51a0603835
Thomas Dybdahl Ahle <thomas@ahle.dk>
2020-09-07 12:15:50 +0200

    Added march=native

--- multshift/runall.sh
+++ multshift/runall.sh
@@ -2,19 +2,20 @@
 
 for compiler in "clang" "gcc"
 do
-    for MA in "" "-DMA"
+    #for MA in "" "-DMA"
+    for MA in "" "-march=native" "-march=native -DMA"
     do
         echo "With $compiler $MA"
         echo "32 Bit"
         $compiler mult_32.c -DK=2 -O3 $MA -DNOP -lm
         ./a.out 2> /dev/null
-        for K in 4 8
+        for K in 3 4
         do
            $compiler mult_32.c -DK=$K -O3 $MA -lm
            ./a.out 2> /dev/null
         done
         echo "64 Bit"
-        for K in 2 4 8
+        for K in 2 3 4
         do
            $compiler mult.c -DK=$K -O3 $MA -lm
            ./a.out 2> /dev/null
